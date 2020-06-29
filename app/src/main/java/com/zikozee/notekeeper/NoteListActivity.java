faef55d9dfb649e232ab01eace98f4862485f01d
Ezekiel Eromosei <ezekiel.eromosei@gmail.com>
2020-06-29 20:33:50 +0100

    Creating the NoteListActivity closes #1.

---
+++ app/src/main/java/com/zikozee/notekeeper/NoteListActivity.java
@@ -0,0 +1,47 @@
+package com.zikozee.notekeeper;
+
+import android.os.Bundle;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.List;
+
+public class NoteListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_note_list);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+        initiateDisplayContent();
+    }
+
+    private void initiateDisplayContent() {
+        ListView listNotes = findViewById(R.id.list_notes);
+
+        List<NoteInfo> notes = DataManager.getInstance().getNotes();
+        ArrayAdapter<NoteInfo> adapterNotes = new ArrayAdapter<>(this,
+                android.R.layout.simple_list_item_1, notes);
+
+        listNotes.setAdapter(adapterNotes);
+    }
+}
