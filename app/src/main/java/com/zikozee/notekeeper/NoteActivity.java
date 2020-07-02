8cfb184272919cb66bd99036aa97b09c217a5812
Ezekiel Eromosei <ezekiel.eromosei@gmail.com>
2020-07-02 21:35:54 +0100

    Implementing note change Cancellation. closes #11

--- app/src/main/java/com/zikozee/notekeeper/NoteActivity.java
+++ app/src/main/java/com/zikozee/notekeeper/NoteActivity.java
@@ -23,6 +23,7 @@ public class NoteActivity extends AppCompatActivity {
     private EditText mTextNoteTitle;
     private EditText mTextNoteText;
     private int mNotePosition;
+    private boolean mIsCancelling;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -50,7 +51,12 @@ public class NoteActivity extends AppCompatActivity {
     @Override
     protected void onPause() {
         super.onPause();
-        saveNote();
+        if(mIsCancelling) {
+            if(mIsNewNote)
+                DataManager.getInstance().removeNote(mNotePosition);
+        }else{
+            saveNote();
+        }
     }
 
     private void saveNote() {
@@ -103,6 +109,9 @@ public class NoteActivity extends AppCompatActivity {
         if (id == R.id.action_send_mail) {
             sendEmail();
             return true;
+        }else if(id== R.id.action_cancel){
+            mIsCancelling = true;
+            finish();
         }
 
         return super.onOptionsItemSelected(item);
