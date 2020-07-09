d3cdc0862e7947590cfc8e2d31abde73de1b6cb5
Ezekiel Eromosei <ezekiel.eromosei@gmail.com>
2020-07-09 04:12:05 +0100

    Restoring a Note's Previous values. closes #13

--- app/src/main/java/com/zikozee/notekeeper/NoteActivity.java
+++ app/src/main/java/com/zikozee/notekeeper/NoteActivity.java
@@ -24,6 +24,9 @@ public class NoteActivity extends AppCompatActivity {
     private EditText mTextNoteText;
     private int mNotePosition;
     private boolean mIsCancelling;
+    private String mOriginalNoteCourseId;
+    private String mOriginalNoteTitle;
+    private String mOriginalNoteText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,6 +43,7 @@ public class NoteActivity extends AppCompatActivity {
         mSpinnerCourses.setAdapter(adapterCourses);
 
         readDisplayStateValues();
+        saveOriginalNoteValues();
 
         mTextNoteTitle = findViewById(R.id.text_note_title);
         mTextNoteText = findViewById(R.id.text_note_text);
@@ -48,15 +52,31 @@ public class NoteActivity extends AppCompatActivity {
             displayNote(mSpinnerCourses, mTextNoteTitle, mTextNoteText);
     }
 
+    private void saveOriginalNoteValues() {
+        if(mIsNewNote) return;
+
+        mOriginalNoteCourseId = mNote.getCourse().getCourseId();
+        mOriginalNoteTitle = mNote.getCourse().getTitle();
+        mOriginalNoteText = mNote.getText();
+    }
+
     @Override
     protected void onPause() {
         super.onPause();
         if(mIsCancelling) {
-            if(mIsNewNote)
-                DataManager.getInstance().removeNote(mNotePosition);
+            if(mIsNewNote) DataManager.getInstance().removeNote(mNotePosition);
+            else storePreviousNoteValues();
+
         }else{
             saveNote();
         }
+    }
+
+    private void storePreviousNoteValues() {
+        CourseInfo course = DataManager.getInstance().getCourse(mOriginalNoteCourseId);
+        mNote.setCourse(course);
+        mNote.setTitle(mOriginalNoteTitle);
+        mNote.setText(mOriginalNoteText);
     }
 
     private void saveNote() {
