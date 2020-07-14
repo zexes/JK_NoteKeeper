335a15a9ac17357ef5e8c026fe52625d37192135
Ezekiel Eromosei <ezekiel.eromosei@gmail.com>
2020-07-14 10:22:10 +0100

    Implementing and Running a Test. closes #18

--- app/src/test/java/com/zikozee/notekeeper/DataManagerTest.java
+++ app/src/test/java/com/zikozee/notekeeper/DataManagerTest.java
@@ -1,5 +1,6 @@
 package com.zikozee.notekeeper;
 
+import org.junit.Assert;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
@@ -8,5 +9,20 @@ public class DataManagerTest {
 
     @Test
     public void createNewNote() {
+        DataManager dm = DataManager.getInstance();
+        final CourseInfo course = dm.getCourse("android_async");
+        final String noteTitle = "Test note title";
+        final String noteText = "This is the body text of my test note";
+
+        int noteIndex = dm.createNewNote();
+        NoteInfo newNote = dm.getNotes().get(noteIndex);
+        newNote.setCourse(course);
+        newNote.setTitle(noteTitle);
+        newNote.setText(noteText);
+
+        NoteInfo compareNote = dm.getNotes().get(noteIndex);
+        assertEquals(compareNote.getCourse(), course);
+        assertEquals(compareNote.getTitle(), noteTitle);
+        assertEquals(compareNote.getText(), noteText);
     }
 }
