9a354ce58b05ee66811bd5aa0475557dc229e1e5
Ezekiel Eromosei <ezekiel.eromosei@gmail.com>
2020-07-14 10:49:32 +0100

    Pre-Processing Between at the Start of Each test class. closes #20

--- app/src/test/java/com/zikozee/notekeeper/DataManagerTest.java
+++ app/src/test/java/com/zikozee/notekeeper/DataManagerTest.java
@@ -2,32 +2,38 @@ package com.zikozee.notekeeper;
 
 import org.junit.Assert;
 import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
 
 public class DataManagerTest {
+    static DataManager sDataManager;
+
+    @BeforeClass
+    public static void beforeClass(){
+        sDataManager = DataManager.getInstance();
+    }
+
     @Before
     public void setUp(){
-        DataManager dm = DataManager.getInstance();
-        dm.getNotes().clear();// clear up before running another unit test
-        dm.initializeExampleNotes();
+        sDataManager.getNotes().clear();// clear up before running another unit test
+        sDataManager.initializeExampleNotes();
     }
 
     @Test
     public void createNewNote() {
-        DataManager dm = DataManager.getInstance();
-        final CourseInfo course = dm.getCourse("android_async");
+        final CourseInfo course = sDataManager.getCourse("android_async");
         final String noteTitle = "Test note title";
         final String noteText = "This is the body text of my test note";
 
-        int noteIndex = dm.createNewNote();
-        NoteInfo newNote = dm.getNotes().get(noteIndex);
+        int noteIndex = sDataManager.createNewNote();
+        NoteInfo newNote = sDataManager.getNotes().get(noteIndex);
         newNote.setCourse(course);
         newNote.setTitle(noteTitle);
         newNote.setText(noteText);
 
-        NoteInfo compareNote = dm.getNotes().get(noteIndex);
+        NoteInfo compareNote = sDataManager.getNotes().get(noteIndex);
         assertEquals(course, compareNote.getCourse());
         assertEquals(noteTitle, compareNote.getTitle());
         assertEquals(noteText, compareNote.getText());
@@ -35,28 +41,27 @@ public class DataManagerTest {
 
     @Test
     public void findSimilarNotes() {
-        DataManager dm = DataManager.getInstance();
-        final CourseInfo course = dm.getCourse("android_async");
+        final CourseInfo course = sDataManager.getCourse("android_async");
         final String noteTitle = "Test note title";
         final String noteText1 = "This is the body text of my test note";
         final String noteText2  = "This is the body of my second test note";
 
-        int noteIndex1 = dm.createNewNote();
-        NoteInfo newNote1 = dm.getNotes().get(noteIndex1);
+        int noteIndex1 = sDataManager.createNewNote();
+        NoteInfo newNote1 = sDataManager.getNotes().get(noteIndex1);
         newNote1.setCourse(course);
         newNote1.setTitle(noteTitle);
         newNote1.setText(noteText1);
 
-        int noteIndex2 = dm.createNewNote();
-        NoteInfo newNote2 = dm.getNotes().get(noteIndex2);
+        int noteIndex2 = sDataManager.createNewNote();
+        NoteInfo newNote2 = sDataManager.getNotes().get(noteIndex2);
         newNote2.setCourse(course);
         newNote2.setTitle(noteTitle);
         newNote2.setText(noteText2);
 
-        int foundIndex1 = dm.findNote(newNote1);
+        int foundIndex1 = sDataManager.findNote(newNote1);
         assertEquals(noteIndex1, foundIndex1);
 
-        int foundIndex2 = dm.findNote(newNote2);
+        int foundIndex2 = sDataManager.findNote(newNote2);
         assertEquals(noteIndex2, foundIndex2);
     }
 }
