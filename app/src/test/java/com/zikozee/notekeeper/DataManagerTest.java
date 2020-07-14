2b169a4b985d1673b8f8a8288a28695ca33e5af0
Ezekiel Eromosei <ezekiel.eromosei@gmail.com>
2020-07-14 09:43:03 +0100

    Adding a unit Test. closes #17

---
+++ app/src/test/java/com/zikozee/notekeeper/DataManagerTest.java
@@ -0,0 +1,12 @@
+package com.zikozee.notekeeper;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class DataManagerTest {
+
+    @Test
+    public void createNewNote() {
+    }
+}
