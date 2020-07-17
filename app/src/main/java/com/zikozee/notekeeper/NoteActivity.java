51749dc071774bc5a974c1e01c14df74aa0ec660
Ezekiel Eromosei <ezekiel.eromosei@gmail.com>
2020-07-17 19:38:19 +0100

    Adding Options Menu Items to our App. closes #34

--- app/src/main/java/com/zikozee/notekeeper/NoteActivity.java
+++ app/src/main/java/com/zikozee/notekeeper/NoteActivity.java
@@ -155,9 +155,15 @@ public class NoteActivity extends AppCompatActivity {
         }else if(id== R.id.action_cancel){
             mIsCancelling = true;
             finish();
+        }else if(id== R.id.action_next){
+            moveNext();
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    private void moveNext() {
+
     }
 
     private void sendEmail() {
