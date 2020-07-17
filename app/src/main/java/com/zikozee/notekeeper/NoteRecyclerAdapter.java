55d1a981c24b8f8fbb62e6da2fd6d5fb8406792f
Ezekiel Eromosei <ezekiel.eromosei@gmail.com>
2020-07-17 18:57:13 +0100

    Creating and Constructing the RecyclerView Adapter. closes #30

---
+++ app/src/main/java/com/zikozee/notekeeper/NoteRecyclerAdapter.java
@@ -0,0 +1,43 @@
+package com.zikozee.notekeeper;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+class NoteRecyclerAdapter extends RecyclerView.Adapter<NoteRecyclerAdapter.ViewHolder>{
+
+    private final Context mContext;
+    private final LayoutInflater mLayoutInflater;
+
+    NoteRecyclerAdapter(Context context) {
+        mContext = context;
+        mLayoutInflater = LayoutInflater.from(mContext);
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder{
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+        }
+    }
+}
