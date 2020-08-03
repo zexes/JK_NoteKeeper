686c27ab415b16f240f2f51b06ea0836a2487cfe
Ezekiel Eromosei <ezekiel.eromosei@gmail.com>
2020-08-03 19:42:40 +0100

    working with value resources. closes #50

--- app/src/main/java/com/zikozee/notekeeper/MainActivity.java
+++ app/src/main/java/com/zikozee/notekeeper/MainActivity.java
@@ -67,7 +67,7 @@ public class MainActivity extends AppCompatActivity
     private void initiateDisplayContent() {
         mRecyclerItems = (RecyclerView) findViewById(R.id.list_items);
         mNoteLayoutManager = new LinearLayoutManager(this);
-        mCoursesLayoutManager = new GridLayoutManager(this, 2);
+        mCoursesLayoutManager = new GridLayoutManager(this, getResources().getInteger(R.integer.course_grid_span));
 
         List<NoteInfo> notes = DataManager.getInstance().getNotes();
         mNoteRecyclerAdapter = new NoteRecyclerAdapter(this, notes);
